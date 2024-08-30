dde56f50da491761d509b8b210a382b3de862fba
Martin Shanahan <m.shanahan914@gmail.com>
2024-08-30 23:01:50 +0100

    adding main file - broadcast

--- client.js
+++ client.js
@@ -1,47 +1,48 @@
-import WebSocket from "ws"
-import readline from 'readline'
+import WebSocket from "ws";
+import readline from "readline";
 
-// cli setup
-const rl = readline.createInterface({
-    input : process.stdin,
-    output: process.stdout
-})
+const startClient = () => {
+  // cli setup
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
 
+  //client
+  const client = new WebSocket("ws://localhost:8080");
 
-//client
-const client = new WebSocket("ws://localhost:8080")
+  // open connection
+  client.on("open", () => {
+    console.log("Connected to server");
 
-// open connection
-client.on('open', () =>{
-    console.log('Connected to server')
-    
     //user input
-    rl.setPrompt('Enter a message:')
+    rl.setPrompt("Enter a message:");
     rl.prompt();
 
-    rl.on('line', (input) => {
-        client.send(input);
-        console.log('Message sent')
-        rl.prompt();
-    })
-})
+    rl.on("line", (input) => {
+      client.send(input);
+      console.log("Message sent");
+      rl.prompt();
+    });
+  });
 
-// from server
-client.on("message", (data)=> {
-    console.log('\nReceived from server:', data.toString())
-    rl.prompt()
-})
+  // from server
+  client.on("message", (data) => {
+    console.log("\nReceived from server:", data.toString());
+    rl.prompt();
+  });
 
-//handle errors
-client.on('error', (error) =>{
-    console.error('Websocket error:', error)
-})
+  //handle errors
+  client.on("error", (error) => {
+    console.error("Websocket error:", error);
+  });
 
-//close connection
-client.on('close', () =>{
-    console.log('\nDisconnected.')
+  //close connection
+  client.on("close", () => {
+    console.log("\nDisconnected.");
     // closes the cli
-    rl.close()
-})
+    rl.close();
+  });
+};
 
-
+export { startClient };
--- server.js
+++ server.js
@@ -1,45 +1,41 @@
 import WebSocket, { WebSocketServer } from "ws";
 
-const server = new WebSocketServer({ port: 8080 });
+const randomUsername = () => {
 
-const activeConnections = [];
+}
 
-const addConnection = (ip) => {
-  if (!activeConnections.includes(ip)) {
-    activeConnections.push(ip);
-  }
-};
+const startServer = () => {
+  const server = new WebSocketServer({ port: 8080 });
 
-const removeConnection = (ip) => {
-  if (activeConnections.includes(ip)) {
-    addConnection = activeConnections.filter((item) => item != ip);
-  }
-};
+  const activeConnections = new Map();
 
-// server
-server.on("connection", function connection(ws, req) {
-
-    // add to active connections 
-  addConnection(req.socket.remoteAddress);
+  // server
+  server.on("connection", function connection(ws, req) {
+    // add to active connections
+    const ip = req.socket.remoteAddress;
+    activeConnections.set(ws, ip);
 
     // handle errors
-  ws.on("error", console.error);
+    ws.on("error", console.error);
 
     //receive and broadcast messages from clients
-  ws.on("message", function message(data) {
-    console.log(req.socket.remoteAddress, " : ", data.toString());
+    ws.on("message", function message(data) {
+      console.log(req.socket.remoteAddress, " : ", data.toString());
 
-    server.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN && client !== ws) {
-        client.send(data);
-      }
+      server.clients.forEach(function each(client) {
+        if (client.readyState === WebSocket.OPEN && client !== ws) {
+          client.send(data);
+        }
+      });
     });
-  });
 
     // client disconnection
-  ws.on("close", (ws, req) => {
-    ip = req.socket.remoteAddress
-    removeConnection(ip);
-    console.log("\n", ip, "disconnected");
+    ws.on("close", (ws) => {
+      const ip = activeConnections.get(ws);
+      activeConnections.delete(ws);
+      console.log("\n", ip, "disconnected");
+    });
   });
-});
+};
+
+export { startServer };
